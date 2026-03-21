9b62e6204563ee3b018aaa6274317475303e630d
沈明辉 <shenminghui@shouqianba.com>
2026-03-21 15:20:38 +0800

    feat: implement Cognitive Complexity visitor in JavaLanguageAdapter

--- src/main/kotlin/com/sqb/complexityradar/adapters/java/JavaLanguageAdapter.kt
+++ src/main/kotlin/com/sqb/complexityradar/adapters/java/JavaLanguageAdapter.kt
@@ -2,8 +2,11 @@ package com.sqb.complexityradar.adapters.java
 
 import com.intellij.psi.JavaRecursiveElementVisitor
 import com.intellij.psi.JavaTokenType
+import com.intellij.psi.PsiBreakStatement
+import com.intellij.psi.PsiCatchSection
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiConditionalExpression
+import com.intellij.psi.PsiContinueStatement
 import com.intellij.psi.PsiDoWhileStatement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiForStatement
@@ -11,7 +14,9 @@ import com.intellij.psi.PsiForeachStatement
 import com.intellij.psi.PsiIfStatement
 import com.intellij.psi.PsiJavaFile
 import com.intellij.psi.PsiMethod
+import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.PsiPolyadicExpression
+import com.intellij.psi.PsiSwitchExpression
 import com.intellij.psi.PsiSwitchLabelStatementBase
 import com.intellij.psi.PsiSwitchStatement
 import com.intellij.psi.PsiTryStatement
@@ -55,6 +60,8 @@ class JavaLanguageAdapter(
         var maxFunctionLoc = 0
         var maxParamCount = 0
         var emptyCatchCount = 0
+        var ccScore = 0
+        var ccNestingDepth = 0
 
         val tokens = mutableListOf<String>()
         val annotations = linkedSetOf<String>()
@@ -103,58 +110,97 @@ class JavaLanguageAdapter(
                 }
 
                 override fun visitIfStatement(statement: PsiIfStatement) {
+                    val isElseContinuation = statement.parent is PsiIfStatement &&
+                        (statement.parent as PsiIfStatement).elseBranch === statement
+                    if (isElseContinuation) {
+                        ccScore += 1
+                    } else {
+                        ccScore += 1 + ccNestingDepth
+                    }
+                    val elseBranch = statement.elseBranch
+                    if (elseBranch != null && elseBranch !is PsiIfStatement) {
+                        ccScore += 1
+                    }
                     branchCount += 1
                     currentDepth += 1
                     maxBlockDepth = maxOf(maxBlockDepth, currentDepth)
                     nestingPenalty = AnalysisSupport.mergePenalty(nestingPenalty, currentDepth)
+                    if (!isElseContinuation) ccNestingDepth += 1
                     super.visitIfStatement(statement)
                     currentDepth -= 1
+                    if (!isElseContinuation) ccNestingDepth -= 1
                 }
 
                 override fun visitSwitchStatement(statement: PsiSwitchStatement) {
+                    ccScore += 1 + ccNestingDepth
                     val labels = statement.body?.statements?.count { it is PsiSwitchLabelStatementBase } ?: 0
                     branchCount += labels.toDouble()
                     currentDepth += 1
                     maxBlockDepth = maxOf(maxBlockDepth, currentDepth)
                     nestingPenalty = AnalysisSupport.mergePenalty(nestingPenalty, currentDepth)
+                    ccNestingDepth += 1
                     super.visitSwitchStatement(statement)
                     currentDepth -= 1
+                    ccNestingDepth -= 1
+                }
+
+                override fun visitSwitchExpression(expression: PsiSwitchExpression) {
+                    ccScore += 1 + ccNestingDepth
+                    currentDepth += 1
+                    maxBlockDepth = maxOf(maxBlockDepth, currentDepth)
+                    nestingPenalty = AnalysisSupport.mergePenalty(nestingPenalty, currentDepth)
+                    ccNestingDepth += 1
+                    super.visitSwitchExpression(expression)
+                    currentDepth -= 1
+                    ccNestingDepth -= 1
                 }
 
                 override fun visitForStatement(statement: PsiForStatement) {
+                    ccScore += 1 + ccNestingDepth
                     loopCount += 1
                     currentDepth += 1
                     maxBlockDepth = maxOf(maxBlockDepth, currentDepth)
                     nestingPenalty = AnalysisSupport.mergePenalty(nestingPenalty, currentDepth)
+                    ccNestingDepth += 1
                     super.visitForStatement(statement)
                     currentDepth -= 1
+                    ccNestingDepth -= 1
                 }
 
                 override fun visitForeachStatement(statement: PsiForeachStatement) {
+                    ccScore += 1 + ccNestingDepth
                     loopCount += 1
                     currentDepth += 1
                     maxBlockDepth = maxOf(maxBlockDepth, currentDepth)
                     nestingPenalty = AnalysisSupport.mergePenalty(nestingPenalty, currentDepth)
+                    ccNestingDepth += 1
                     super.visitForeachStatement(statement)
                     currentDepth -= 1
+                    ccNestingDepth -= 1
                 }
 
                 override fun visitWhileStatement(statement: PsiWhileStatement) {
+                    ccScore += 1 + ccNestingDepth
                     loopCount += 1
                     currentDepth += 1
                     maxBlockDepth = maxOf(maxBlockDepth, currentDepth)
                     nestingPenalty = AnalysisSupport.mergePenalty(nestingPenalty, currentDepth)
+                    ccNestingDepth += 1
                     super.visitWhileStatement(statement)
                     currentDepth -= 1
+                    ccNestingDepth -= 1
                 }
 
                 override fun visitDoWhileStatement(statement: PsiDoWhileStatement) {
+                    ccScore += 1 + ccNestingDepth
                     loopCount += 1
                     currentDepth += 1
                     maxBlockDepth = maxOf(maxBlockDepth, currentDepth)
                     nestingPenalty = AnalysisSupport.mergePenalty(nestingPenalty, currentDepth)
+                    ccNestingDepth += 1
                     super.visitDoWhileStatement(statement)
                     currentDepth -= 1
+                    ccNestingDepth -= 1
                 }
 
                 override fun visitTryStatement(statement: PsiTryStatement) {
@@ -165,8 +211,15 @@ class JavaLanguageAdapter(
                     currentDepth += 1
                     maxBlockDepth = maxOf(maxBlockDepth, currentDepth)
                     nestingPenalty = AnalysisSupport.mergePenalty(nestingPenalty, currentDepth)
+                    ccNestingDepth += 1
                     super.visitTryStatement(statement)
                     currentDepth -= 1
+                    ccNestingDepth -= 1
+                }
+
+                override fun visitCatchSection(section: PsiCatchSection) {
+                    ccScore += 1 + (ccNestingDepth - 1).coerceAtLeast(0)
+                    super.visitCatchSection(section)
                 }
 
                 override fun visitConditionalExpression(expression: PsiConditionalExpression) {
@@ -175,8 +228,10 @@ class JavaLanguageAdapter(
                 }
 
                 override fun visitPolyadicExpression(expression: PsiPolyadicExpression) {
-                    if (expression.operationTokenType == JavaTokenType.ANDAND || expression.operationTokenType == JavaTokenType.OROR) {
+                    val op = expression.operationTokenType
+                    if (op == JavaTokenType.ANDAND || op == JavaTokenType.OROR) {
                         logicalOpCount += (expression.operands.size - 1).coerceAtLeast(1)
+                        ccScore += 1
                     }
                     super.visitPolyadicExpression(expression)
                 }
@@ -212,6 +267,7 @@ class JavaLanguageAdapter(
             annotations = annotations,
             superTypes = superTypes,
             classNames = classNames,
+            cognitiveComplexity = ccScore,
         )
     }
 
@@ -233,13 +289,13 @@ class JavaLanguageAdapter(
             .map { method ->
                 val metrics = collectMethodMetrics(method)
                 scorer.scoreHotspot(
-                    methodName = method.name,
-                    line = AnalysisSupport.lineNumber(file, method.nameIdentifier ?: method),
-                    length = metrics.length,
-                    controlFlow = metrics.controlFlow,
-                    nestingPenalty = metrics.nestingPenalty,
-                    snippet = AnalysisSupport.snippet(method),
-                    config = config,
+                    method.name,
+                    AnalysisSupport.lineNumber(file, method.nameIdentifier ?: method),
+                    metrics.length,
+                    metrics.cognitiveComplexity.toDouble(),
+                    metrics.nestingPenalty,
+                    AnalysisSupport.snippet(method),
+                    config,
                 )
             }.sortedByDescending { it.score }
             .take(config.hotspot.maxHotspotsPerFile)
@@ -257,13 +313,13 @@ class JavaLanguageAdapter(
             .map { method ->
                 val metrics = collectMethodMetrics(method)
                 scorer.scoreHotspot(
-                    methodName = method.name,
-                    line = AnalysisSupport.lineNumber(file, method.nameIdentifier ?: method),
-                    length = metrics.length,
-                    controlFlow = metrics.controlFlow,
-                    nestingPenalty = metrics.nestingPenalty,
-                    snippet = AnalysisSupport.snippet(method),
-                    config = config,
+                    method.name,
+                    AnalysisSupport.lineNumber(file, method.nameIdentifier ?: method),
+                    metrics.length,
+                    metrics.cognitiveComplexity.toDouble(),
+                    metrics.nestingPenalty,
+                    AnalysisSupport.snippet(method),
+                    config,
                 )
             }.sortedByDescending { it.score }
             .take(config.hotspot.maxHotspotsPerFile)
@@ -271,104 +327,145 @@ class JavaLanguageAdapter(
     }
 
     private fun collectMethodMetrics(method: PsiMethod): JavaMethodMetrics {
-        var branchCount = 0.0
-        var loopCount = 0
-        var tryCatchCount = 0
-        var ternaryCount = 0
-        var logicalOpCount = 0
+        var ccScore = 0
+        var ccDepth = 0
         var currentDepth = 0
         var maxDepth = 0
         var penalty = 0
+        val methodName = method.name
 
         method.accept(
             object : JavaRecursiveElementVisitor() {
                 override fun visitIfStatement(statement: PsiIfStatement) {
-                    branchCount += 1
-                    currentDepth += 1
-                    maxDepth = maxOf(maxDepth, currentDepth)
-                    penalty = AnalysisSupport.mergePenalty(penalty, currentDepth)
+                    val isElseContinuation = statement.parent is PsiIfStatement &&
+                        (statement.parent as PsiIfStatement).elseBranch === statement
+                    if (isElseContinuation) {
+                        ccScore += 1
+                    } else {
+                        ccScore += 1 + ccDepth
+                    }
+                    val elseBranch = statement.elseBranch
+                    if (elseBranch != null && elseBranch !is PsiIfStatement) {
+                        ccScore += 1
+                    }
+                    if (!isElseContinuation) {
+                        ccDepth += 1
+                        currentDepth += 1
+                        maxDepth = maxOf(maxDepth, currentDepth)
+                        penalty = AnalysisSupport.mergePenalty(penalty, currentDepth)
+                    }
                     super.visitIfStatement(statement)
-                    currentDepth -= 1
+                    if (!isElseContinuation) {
+                        ccDepth -= 1
+                        currentDepth -= 1
+                    }
                 }
 
                 override fun visitSwitchStatement(statement: PsiSwitchStatement) {
-                    branchCount += (statement.body?.statements?.count { it is PsiSwitchLabelStatementBase } ?: 0).toDouble()
+                    ccScore += 1 + ccDepth
+                    ccDepth += 1
                     currentDepth += 1
                     maxDepth = maxOf(maxDepth, currentDepth)
                     penalty = AnalysisSupport.mergePenalty(penalty, currentDepth)
                     super.visitSwitchStatement(statement)
+                    ccDepth -= 1
+                    currentDepth -= 1
+                }
+
+                override fun visitSwitchExpression(expression: PsiSwitchExpression) {
+                    // Java 14+ switch expression
+                    ccScore += 1 + ccDepth
+                    ccDepth += 1
+                    currentDepth += 1
+                    maxDepth = maxOf(maxDepth, currentDepth)
+                    penalty = AnalysisSupport.mergePenalty(penalty, currentDepth)
+                    super.visitSwitchExpression(expression)
+                    ccDepth -= 1
                     currentDepth -= 1
                 }
 
                 override fun visitForStatement(statement: PsiForStatement) {
-                    loopCount += 1
-                    currentDepth += 1
+                    ccScore += 1 + ccDepth
+                    ccDepth += 1; currentDepth += 1
                     maxDepth = maxOf(maxDepth, currentDepth)
                     penalty = AnalysisSupport.mergePenalty(penalty, currentDepth)
                     super.visitForStatement(statement)
-                    currentDepth -= 1
+                    ccDepth -= 1; currentDepth -= 1
                 }
 
                 override fun visitForeachStatement(statement: PsiForeachStatement) {
-                    loopCount += 1
-                    currentDepth += 1
+                    ccScore += 1 + ccDepth
+                    ccDepth += 1; currentDepth += 1
                     maxDepth = maxOf(maxDepth, currentDepth)
                     penalty = AnalysisSupport.mergePenalty(penalty, currentDepth)
                     super.visitForeachStatement(statement)
-                    currentDepth -= 1
+                    ccDepth -= 1; currentDepth -= 1
                 }
 
                 override fun visitWhileStatement(statement: PsiWhileStatement) {
-                    loopCount += 1
-                    currentDepth += 1
+                    ccScore += 1 + ccDepth
+                    ccDepth += 1; currentDepth += 1
                     maxDepth = maxOf(maxDepth, currentDepth)
                     penalty = AnalysisSupport.mergePenalty(penalty, currentDepth)
                     super.visitWhileStatement(statement)
-                    currentDepth -= 1
+                    ccDepth -= 1; currentDepth -= 1
                 }
 
                 override fun visitDoWhileStatement(statement: PsiDoWhileStatement) {
-                    loopCount += 1
-                    currentDepth += 1
+                    ccScore += 1 + ccDepth
+                    ccDepth += 1; currentDepth += 1
                     maxDepth = maxOf(maxDepth, currentDepth)
                     penalty = AnalysisSupport.mergePenalty(penalty, currentDepth)
                     super.visitDoWhileStatement(statement)
-                    currentDepth -= 1
+                    ccDepth -= 1; currentDepth -= 1
                 }
 
                 override fun visitTryStatement(statement: PsiTryStatement) {
-                    tryCatchCount += statement.catchSections.size
-                    currentDepth += 1
+                    // try itself adds no CC, only increases depth for catch
+                    ccDepth += 1; currentDepth += 1
                     maxDepth = maxOf(maxDepth, currentDepth)
                     penalty = AnalysisSupport.mergePenalty(penalty, currentDepth)
                     super.visitTryStatement(statement)
-                    currentDepth -= 1
+                    ccDepth -= 1; currentDepth -= 1
                 }
 
-                override fun visitConditionalExpression(expression: PsiConditionalExpression) {
-                    ternaryCount += 1
-                    super.visitConditionalExpression(expression)
+                override fun visitCatchSection(section: PsiCatchSection) {
+                    // use outer depth (ccDepth - 1) to avoid double-counting try's depth increment
+                    ccScore += 1 + (ccDepth - 1).coerceAtLeast(0)
+                    super.visitCatchSection(section)
                 }
 
                 override fun visitPolyadicExpression(expression: PsiPolyadicExpression) {
-                    if (expression.operationTokenType == JavaTokenType.ANDAND || expression.operationTokenType == JavaTokenType.OROR) {
-                        logicalOpCount += (expression.operands.size - 1).coerceAtLeast(1)
+                    val op = expression.operationTokenType
+                    if (op == JavaTokenType.ANDAND || op == JavaTokenType.OROR) {
+                        // PsiPolyadicExpression already groups same-operator sequences
+                        ccScore += 1
                     }
                     super.visitPolyadicExpression(expression)
+                }
+
+                override fun visitMethodCallExpression(expression: PsiMethodCallExpression) {
+                    if (expression.methodExpression.referenceName == methodName) {
+                        ccScore += 1  // recursion
+                    }
+                    super.visitMethodCallExpression(expression)
+                }
+
+                override fun visitBreakStatement(statement: PsiBreakStatement) {
+                    if (statement.labelIdentifier != null) ccScore += 1  // break with label
+                    super.visitBreakStatement(statement)
+                }
+
+                override fun visitContinueStatement(statement: PsiContinueStatement) {
+                    if (statement.labelIdentifier != null) ccScore += 1  // continue with label
+                    super.visitContinueStatement(statement)
                 }
             },
         )
 
-        val controlFlow = AnalysisSupport.computeControlFlow(
-            branchCount = branchCount,
-            loopCount = loopCount,
-            tryCatchCount = tryCatchCount,
-            ternaryCount = ternaryCount,
-            logicalOpCount = logicalOpCount,
-        )
         return JavaMethodMetrics(
             length = AnalysisSupport.effectiveLoc(method.text),
-            controlFlow = controlFlow,
+            cognitiveComplexity = ccScore,
             nestingPenalty = maxOf(penalty, AnalysisSupport.nestingPenalty(maxDepth)),
         )
     }
@@ -377,6 +474,6 @@ class JavaLanguageAdapter(
 
 private data class JavaMethodMetrics(
     val length: Int,
-    val controlFlow: Double,
+    val cognitiveComplexity: Int,
     val nestingPenalty: Int,
 )
